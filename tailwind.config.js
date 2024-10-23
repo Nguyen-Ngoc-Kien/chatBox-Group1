592ccf8d5825aa64e51e6ee3f92da7bd78325bad
Kien <ngngockien2002@gmail.com>
2024-10-23 17:04:43 +0700

    Update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {      
+      maxWidth: {
+      'screen-sm': '640px',
+      'screen-md': '768px',
+      'screen-lg': '1024px',
+      'screen-xl': '1280px',
+      'screen-2xl': '1536px',
+    },
   },
+},
   plugins: [],
   container: {
       center: true,
