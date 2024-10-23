86e409148a8af98bf2dbfd0bd651cfbc3e5bf389
Kien <ngngockien2002@gmail.com>
2024-10-23 11:38:46 +0700

    chatBox

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  container: {
+      center: true,
+      screens: {
+        'xl': '1440px',
+      }
+    },
+}
